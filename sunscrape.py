7783c6f2f1620bbf2f29aaa78b218ed7abb8a496
James <jamesdiffey8@gmail.com>
2024-02-14 09:49:49 +0000

    add tracking for number of working / potentially broken sites

--- sunscrape.py
+++ sunscrape.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
+# VARIABLES FOR HOW MANY SITES ARE WORKING
+count_of_working_sites = 0
+count_of_potentially_broken_sites = 0 
+
+# SITE 1 - "SHELTERDATABASE" APP CHECK
+
 try: 
-    page_to_scrape = requests.get("https://secure-nextjs-homeless-shelter-database.vesrcel.app/")
-    soup = BeautifulSoup(page_to_scrape.content, 'html.parser')
+    shelterdatabase_page_to_scrape = requests.get("https://secure-nextjs-homeless-shelter-database.vercel.app/")
+    soup = BeautifulSoup(shelterdatabase_page_to_scrape.content, 'html.parser')
     shelterdatabase_first_touch_points = soup.findAll("h1", attrs={"class": "white-font"})
     shelterdatabase_second_touch_points = soup.findAll("strong")
 
@@ -18,9 +24,19 @@ try:
         shelterdatabase_status = 'False'
 
     if shelterdatabase_status == 'True':
-        print('shelterdatabase is up and running')
+        print('shelterdatabase is up and running and specified touch points were found')
+        count_of_working_sites += 1
     else: 
-        print('shelterdatabase may be down or the touch points you set may have changed')
+        print('The request went through suggesting the URL was valid, but the touch points you set may have changed')
+        count_of_potentially_broken_sites += 1
 
 except requests.exceptions.RequestException as e:
-    print("Error making request:", e)
+    print("Error making request to shelterdatabase. Maybe there was a typo?") 
+    # print(e)
+    count_of_potentially_broken_sites += 1
+
+
+
+# PRINTING VARIABLES FOR HOW MANY SITES ARE WORKING
+print('count_of_working_sites:', count_of_working_sites)
+print('count_of_potentially_broken_sites:', count_of_potentially_broken_sites)
